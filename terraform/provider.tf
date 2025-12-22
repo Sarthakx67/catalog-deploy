bdca8431f599686adf65c597dc712f52243b719a
sarthak <sarthakx67@gmail.com>
2025-12-22 18:47:52 +0530

    updated some files

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,16 +3,9 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
+        }
     }
-  }
-  backend "s3" {
-    bucket   = "sarthak-remote-tfstate"
-    key = "catalogue-dev"
-    region = "ap-south-1"
-    dynamodb_table = "sarthak-tfstate-lock"
-  }
 }
-
 provider "aws" {
   region = "ap-south-1"
 }
